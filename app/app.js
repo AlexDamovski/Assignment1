f10b463d54b8bb5da99a287a9da6b670af1ee482
AlexDamovski <adamovsk@my.centennialcollege.ca>
2022-09-25 15:48:17 -0400

    Added custom logo

--- app/app.js
+++ app/app.js
@@ -7,7 +7,8 @@ import session from 'express-session';
 // ES Modules fix for __dirname
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(
+    import.meta.url));
 
 //Configuration Module
 import { Secret } from '../config/config.js';
@@ -26,13 +27,14 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname,'/client')));
-app.use(express.static(path.join(__dirname,'../public')));
+// app.use(express.static(path.join(__dirname, './public')));
+app.use('/public', express.static('public'));
 app.use(session({
     secret: Secret,
-    saveUninitialized: false, 
+    saveUninitialized: false,
     resave: false
 }));
 
@@ -41,4 +43,3 @@ app.use(session({
 app.use('/', indexRouter);
 
 export default app;
-
