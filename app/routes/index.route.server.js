212d6803fc475d005d29a1896d6884a4faf65a58
AlexDamovski <adamovsk@my.centennialcollege.ca>
2022-09-25 16:38:12 -0400

    Updated npm modules and fixed route bug

--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { displayContactPage, displayHomePage, displayProjectsPage, displayServicesPage } from "../controllers/index.controller.server.js";
+import { displayContactPage, displayHomePage, displayProjectsPage, displayServicesPage, displayAboutPage } from "../controllers/index.controller.server.js";
 
 const router = Router();
 
 router.get('/', displayHomePage);
 router.get('/home', displayHomePage);
-router.get('/about', displayHomePage);
+router.get('/about', displayAboutPage);
 router.get('/projects', displayProjectsPage);
 router.get('/services', displayServicesPage);
 router.get('/contact', displayContactPage);
